c15bed3a4441cc8b2492466f66c7740df80668f3
saasbook <saasbook@saasbook.(none)>
2015-09-18 17:34:59 -0300

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  root 'welcome#index'
 
   resources :movies
   
